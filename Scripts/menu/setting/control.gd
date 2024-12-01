50a06c4bc61734a282ffacde6a87795d7cd8e04d
Matheo Travers <matheo.daemon@gmail.com>
2024-12-01 16:29:38 +0100

    Fix: Complement documentation et reorganise le code

--- Scripts/menu/setting/control.gd
+++ Scripts/menu/setting/control.gd
@@ -1,35 +1,12 @@
 extends Control
 
-@onready var change_avancer = $ColorRect/Control/Avancer_Button
-
-@onready var change_reculer = $ColorRect/Control/Reculer_Button
-
-@onready var change_gauche = $ColorRect/Control/Gauche_Button
-
-@onready var change_droit = $ColorRect/Control/Droite_Button
-
-@onready var change_saut = $ColorRect/Control/Sauter_Button
-
-@onready var change_courir = $ColorRect/Control/Courir_Button
-
-@onready var change_vue = $ColorRect/Control/Vue_Button
-
-@onready var change_accroupie = $ColorRect/Control/accroupie_Button
-
-@onready var change_interagir = $ColorRect/Control/interagir_Button
-
-@onready var change_inventaire = $ColorRect/Control/inventaire_Button
-
-@onready var change_carte = $ColorRect/Control/carte_Button
-
-@onready var change_marcher = $ColorRect/Control/marcher_Button
-
-@onready var change_ramper = $ColorRect/Control/ramper_Button
-
+## Si ecoute le clavier
 var analyser_entree : bool = false
 
+## Index du bouton cliquer
 var select_button_index : int
 
+## Liste des touches clavier par défauts
 const controle : Array = [
 	["Z", KEY_W],
 	["S", KEY_S],
@@ -46,6 +23,7 @@ const controle : Array = [
 	["C", KEY_C]
 ]
 
+## Liste des touches manettes par défauts
 const controle_manette : Array = [
 	["A", JOY_BUTTON_A],
 	["L3", JOY_BUTTON_LEFT_STICK],
@@ -57,6 +35,7 @@ const controle_manette : Array = [
 	["Flèche bas", JOY_BUTTON_DPAD_DOWN]
 ]
 
+## Liste des boutons de controles
 @onready var boutons : Array = [
 	$ColorRect/Control/Avancer_Button,
 	$ColorRect/Control/Reculer_Button,
@@ -82,19 +61,19 @@ func _ready() -> void:
 func add_name_for_button(key : Array, button : Button):
 	button.text = key[0]
 
-##Fonction pour désactiver tout les boutons
+## Fonction pour désactiver l'interaction de tout les boutons
 func disabled_all_boutton(time_sleep: float = 0.5):
 	await get_tree().create_timer(time_sleep).timeout
 	for bouton in boutons:
 		bouton.disabled = true
 
-##Fonction pour activer tout les boutons
+## Fonction pour activer l'interaction de tout les boutons
 func enabled_all_boutton(time_sleep: float = 0.5 ):
 	await get_tree().create_timer(time_sleep).timeout
 	for bouton in boutons:
 		bouton.disabled = false
 
-##Fonction pour afficher lors d'un appuis sur le bouton le texte pour changer de touche
+## Fonction pour afficher lors d'un appuis sur le bouton le texte pour changer de touche
 func change_button_for_control(change_button : Button):
 	if change_button.pressed and not analyser_entree:
 		change_button.text = "Entrer une touche"
@@ -102,65 +81,69 @@ func change_button_for_control(change_button : Button):
 		disabled_all_boutton(0)
 
 func _on_avancer_button_pressed() -> void:
-	change_button_for_control(change_avancer)
-	select_button_index = 0
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 	
 func _on_reculer_button_pressed() -> void:
-	change_button_for_control(change_reculer)
-	select_button_index = 1
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 
 func _on_gauche_button_pressed() -> void:
-	change_button_for_control(change_gauche)
-	select_button_index = 2
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 
 func _on_droite_button_pressed() -> void:
-	change_button_for_control(change_droit)
-	select_button_index = 3
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 
 func _on_sauter_button_pressed() -> void:
-	change_button_for_control(change_saut)
-	select_button_index = 4
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 
 func _on_courir_button_pressed() -> void:
-	change_button_for_control(change_courir)
-	select_button_index = 5
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 
 func _on_vue_button_pressed() -> void:
-	change_button_for_control(change_vue)
-	select_button_index = 6
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 
 func _on_accroupie_button_pressed() -> void:
-	change_button_for_control(change_accroupie)
-	select_button_index = 7
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 
 func _on_interagir_button_pressed() -> void:
-	change_button_for_control(change_interagir)
-	select_button_index = 8
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 
 func _on_inventaire_button_pressed() -> void:
-	change_button_for_control(change_inventaire)
-	select_button_index = 9
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 
 func _on_carte_button_pressed() -> void:
-	change_button_for_control(change_carte)
-	select_button_index = 10
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 	
 func _on_marcher_button_pressed() -> void:
-	change_button_for_control(change_marcher)
-	select_button_index = 11
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 	
 func _on_ramper_button_pressed() -> void:
-	change_button_for_control(change_ramper)
-	select_button_index = 12
-
-##Fonction pour changer les touches
-func _input(event: InputEvent) -> void:
-	if event.is_pressed() and analyser_entree:
-		var thread = Thread.new()
-		boutons[select_button_index].text = event.as_text()
-		analyser_entree = false
-		thread.start(Callable(self, "enabled_all_boutton"))
-		
+	var index: int = 0
+	change_button_for_control(boutons[index])
+	select_button_index = index
 
 func _on_graphisme_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu/setting/video.tscn")
@@ -180,3 +163,15 @@ func _on_retour_button_pressed() -> void:
 
 func _on_sensibilite_souris_value_changed(value: float) -> void:
 	pass # Replace with function body.
+
+## Méthode hériter qui récupères les touches
+## A l'intérieurs on retrouve la verification
+## si une touche et appuyer et que le clavier et écouter.
+func _input(event: InputEvent) -> void:
+	if event.is_pressed() and analyser_entree:
+		var thread = Thread.new()
+		boutons[select_button_index].text = event.as_text()
+		analyser_entree = false
+		# Appelle différer
+		thread.start(Callable(self, "enabled_all_boutton"))
+		
