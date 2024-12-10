a0c3d8fee2e461c3539731e14f42b486e5db9014
LoevanCailleaud <cailleaudloevan@gmail.com>
2024-12-10 15:38:10 +0100

    Update analyse fichier

--- Scripts/menu/main_menu.gd
+++ Scripts/menu/main_menu.gd
@@ -8,23 +8,22 @@ var test : Array =[
 
 func _ready():
 	SceneSound.launch_music_menu()
-	analyse_file_and_folder("")
+	search_rercursif_file("")
 	add_name_for_button_test(test, test_button)
 
-func analyse_file_and_folder(sub_folder : String):
+func search_rercursif_file(sub_folder : String):
 	var dir = DirAccess.open("res://Scenes/test/{0}".format([sub_folder]))
 	if dir:
 		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
+		var file_and_folder_name = dir.get_next()
+		while file_and_folder_name != "":
 			if dir.current_is_dir():
-				analyse_file_and_folder("{0}/{1}".format([sub_folder, file_name]))
-				print("hello wourld")
+				search_rercursif_file("{0}/{1}".format([sub_folder, file_and_folder_name]))
 			else:
-				test.push_back("{0}/{1}".format([sub_folder, file_name]))
-			file_name = dir.get_next()
+				test.push_back("{0}/{1}".format([sub_folder, file_and_folder_name]))
+			file_and_folder_name = dir.get_next()
 	else:
-		print("An error occurred when trying to access the path.")
+		push_error("An error occurred when trying to access the path.")
 
 func add_name_for_button_test(name_items : Array, option_button : OptionButton):
 	for name_item in name_items:
