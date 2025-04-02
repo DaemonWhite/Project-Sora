86ac1879111390063033b829a979d0eec9dc537d
DaemonWhite <matheo.daemon@gmail.com>
2025-04-02 12:59:54 +0200

    Ajout: de goup à data settings

--- Scripts/settings/data_settings.gd
+++ Scripts/settings/data_settings.gd
@@ -4,11 +4,16 @@ extends Object
 var name_settings: String = ""
 var default: Variant
 var list: Array
+var group: SoraSettings.GROUP
 
-func _init(name: String, default_settings: Variant, list_settings: Array) -> void:
-	name_settings = name
-	default = default_settings
-	list_settings = list_settings
+func _init(group:  SoraSettings.GROUP, name: String, default_settings: Variant, list_settings: Array) -> void:
+	self.group = group 
+	self.name_settings = name
+	self.default = default_settings
+	self.list = list_settings
+	
+func get_group() -> SoraSettings.GROUP :
+	return self.group
 
 func get_name() -> String :
 	return name_settings
