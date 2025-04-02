fd365d8475ccd7f88ba89107e79672232a84d943
DaemonWhite <matheo.daemon@gmail.com>
2025-04-02 13:01:44 +0200

    Implementation GROUP

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -12,24 +12,28 @@ func _ready() -> void:
 	print(default_settings.VSYNC)
 	
 	settings.add_settings(
+		settings.GROUP.GRAPHICS,
 		"resolution", 
 		default_settings.get_RESOLUTION_DEFAULT(),
 		default_settings.RESOLUTIONS
 	)
 	
 	settings.add_settings(
+		settings.GROUP.GRAPHICS,
 		"window_mode", 
 		default_settings.WINDOW_MODE_DEFAULT,
 		default_settings.WINDOW_MODE
 	)
 	
 	settings.add_settings(
+		settings.GROUP.GRAPHICS,
 		"MSAA", 
 		default_settings.MSAA_DEFAULT,
 		default_settings.MSAA
 	)
 	
 	settings.add_settings(
+		settings.GROUP.GRAPHICS,
 		"vsync", 
 		default_settings.VSYNC_DEFAULT,
 		default_settings.VSYNC
