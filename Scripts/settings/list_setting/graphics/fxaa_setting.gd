8dacfebcbb41899df1c8c554b706a2ebeef6dbd1
DaemonWhite <daemonwhites@vivaldi.net>
2025-04-15 20:40:15 +0200

    Fix: syntax

--- Scripts/settings/list_setting/graphics/fxaa_setting.gd
+++ Scripts/settings/list_setting/graphics/fxaa_setting.gd
@@ -1,11 +1,11 @@
 class_name FxaaSetting
 extends BooleanOptionSettings
 
-func _ready():
+func _ready() -> void:
 	self._name = "Fxaa"
 	self._group = BaseSettings.GROUP.GRAPHICS
 
 	self._default_option = true
 
-func _apply():
+func _apply() -> void:
 	ProjectSettings.set_setting("rendering/anti_aliasing/quality/screen_space_aa", self._current_option)
