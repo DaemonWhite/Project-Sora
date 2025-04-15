69e088a718b2a2f04f2404323bc856df91336599
DaemonWhite <daemonwhites@vivaldi.net>
2025-04-15 14:56:04 +0200

    Fix: not list all keys

--- Scripts/settings/core/key_settings.gd
+++ Scripts/settings/core/key_settings.gd
@@ -8,17 +8,19 @@ func _init(action: String, keycodes: Array[InputEvent] ) -> void:
 	self._default_option = []
 	
 	for keycode in keycodes:
-		self._default_option = keycode.as_text()
+		self._default_option.append(keycode.as_text())
 		
 	super._init()
-	
+
+## Ajoute une lettre à la configuration
 func add_keycode(key: Key) -> void:
 	var input = InputEventKey.new()
 	
 	input.keycode = key
 	
 	InputMap.action_get_events(self._name).append(input)
-	
+
+## Recupères tout les lettres associer à l'evenement
 func get_keycodes() -> Array[Key]:
 	var list_keycode: Array[Key]
 	
