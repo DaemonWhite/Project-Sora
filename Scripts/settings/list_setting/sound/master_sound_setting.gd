bd97d0d0bcbbcbf34002761a7493f9e9fdba4bb7
DaemonWhite <daemonwhites@vivaldi.net>
2025-04-21 17:14:12 +0200

    Add: sound master support

--- Scripts/settings/list_setting/sound/master_sound_setting.gd
+++ Scripts/settings/list_setting/sound/master_sound_setting.gd
@@ -4,11 +4,14 @@ extends SlideOptionSettings
 ## @experimental
 ## TODO Apply n'est pas implementer car fonction inexistante
 
-func _ready():
-    self._name = "MASTER"
-    self._group = BaseSettings.GROUP.SOUND
+func _ready() -> void:
+	self._name = "MASTER"
+	self._group = BaseSettings.GROUP.SOUND
 
-    self._min = 0.0
-    self._max = 1.0
+	self._min = 0.0
+	self._max = 1.0
 
-    self._default_option = self._max
+	self._default_option = 0.2
+	
+func _apply() -> void:
+	SceneSound.update_music_level(self._current_option)
