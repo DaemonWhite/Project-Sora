f81cb5bfd2130824001ec690b592f1be5ce85690
DaemonWhite <daemonwhites@vivaldi.net>
2025-05-08 17:20:56 +0200

    Add: Pause Menu

---
+++ Scripts/menu/pause_menu.gd
@@ -0,0 +1,29 @@
+class_name PauseMenu
+extends Overlay
+
+func _ready() -> void:
+	super._ready()
+	get_tree().paused = !self.visible
+
+func _input(event: InputEvent) -> void:
+	super._input(event)
+	if event.is_action_pressed("Pause"):
+		self.pause()
+
+func pause() -> void:
+	self.visible = !self.visible
+	get_tree().paused = self.visible
+	print(get_tree().paused)
+
+func _on_continue_button_pressed() -> void:
+	self.visible = false
+
+func _on_exit_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/menu/main_menu.tscn")
+
+func _on_option_button_pressed() -> void:
+	self.select_tab_by_name("Options")
+
+func _on_return_button_pressed() -> void:
+	self.select_tab_by_name("Pause")
