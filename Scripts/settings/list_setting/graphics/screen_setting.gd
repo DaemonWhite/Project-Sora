d3153ce2b09c22e4a1d5166ae1be6e7de13040f9
DaemonWhite <daemonwhites@vivaldi.net>
2026-07-14 12:42:40 +0200

    fix: Changement d'écran non fonctionel
    Le changement d'écran fonctione dans tout les cas.

--- Scripts/settings/list_setting/graphics/screen_setting.gd
+++ Scripts/settings/list_setting/graphics/screen_setting.gd
@@ -13,5 +13,26 @@ func _ready() -> void:
 	for i in DisplayServer.get_screen_count():
 		self._options.get_or_add(i,i)
 
-func _apply():
-	DisplayServer.window_set_current_screen(self._options[self._current_option], 0)
+func _apply() -> void:
+	var target_screen = self._options[self._current_option]
+	
+	var current_mode = DisplayServer.window_get_mode(0)
+	
+	# Si le joueur est en mode plein écran, on passe en mode fenêtré pour pouvoir changer d'écran
+	if current_mode == DisplayServer.WINDOW_MODE_FULLSCREEN or current_mode == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED, 0)
+	
+	DisplayServer.window_set_current_screen(target_screen, 0)
+	
+	# Restore le mode d'affichage précédent si le joueur était en mode plein écran ou exclusif plein écran, sinon on recentre la fenêtre sur le nouvel écran
+	if current_mode == DisplayServer.WINDOW_MODE_FULLSCREEN or current_mode == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+		DisplayServer.window_set_mode(current_mode, 0)
+		
+	else:
+		# Si le joueur est en mode fenêtré, on recentre la fenêtre sur le nouvel écran
+		var screen_size = DisplayServer.screen_get_size(target_screen)
+		var window_size = DisplayServer.window_get_size(0)
+		var screen_position = DisplayServer.screen_get_position(target_screen)
+		
+		var new_pos = screen_position + ((screen_size - window_size) / 2)
+		DisplayServer.window_set_position(new_pos, 0)
