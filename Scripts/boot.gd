d2d8339ba16aac09b2f9b209381a56d3a2659669
DaemonWhite <daemonwhites@vivaldi.net>
2026-07-18 17:40:38 +0200

    add: Fix boot path

--- Scripts/boot.gd
+++ Scripts/boot.gd
@@ -44,7 +44,7 @@ func _load_settings() -> void:
 
 func _register_ui_manager() -> void:
 	UiManager.register_ui(&"MainMenu", "res://Scenes/menu/main_menu.tscn", UiManager.LayerType.GAME_MENU)
-	UiManager.register_ui(&"PauseMenu", "/home/matheo/Projects/godot/Project-Sora/Scenes/menu/PauseMenu.tscn", UiManager.LayerType.SYSTEM_MENU)
+	UiManager.register_ui(&"PauseMenu", "res://Scenes/menu/PauseMenu.tscn", UiManager.LayerType.SYSTEM_MENU)
 	UiManager.register_ui(&"SettingsMenu", "res://Scenes/menu/menu_setting.tscn", UiManager.LayerType.SYSTEM_MENU)
 	UiManager.register_ui(&"Dialog", "res://Scenes/menu/components/modal/Dialog.tscn", UiManager.LayerType.SYSTEM_MENU, false)
 	UiManager.register_ui(&"DialogConfirm", "res://Scenes/menu/components/modal/DialogConfirm.tscn", UiManager.LayerType.SYSTEM_MENU, false)
