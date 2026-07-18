ada559be896c9149c509d415c27467f2943de52e
DaemonWhite <daemonwhites@vivaldi.net>
2026-07-18 10:41:19 +0200

    fix: Anti Arcelement de debogueur

--- Scripts/game_signals.gd
+++ Scripts/game_signals.gd
@@ -1,14 +1,24 @@
+class_name Game_Sginals
 extends Node
 
+@warning_ignore("unused_signal")
 signal pause_opened()
+@warning_ignore("unused_signal")
 signal pause_closed()
 
+@warning_ignore("unused_signal")
 signal inventory_opened()
+@warning_ignore("unused_signal")
 signal inventory_closed()
 
+@warning_ignore("unused_signal")
 signal loading_game(state: GameStateManager.State, load_file: String)
+@warning_ignore("unused_signal")
 signal loaded_game(state: GameStateManager.State)
 
+@warning_ignore("unused_signal")
 signal saved_game()
+@warning_ignore("unused_signal")
 signal closed_game()
+@warning_ignore("unused_signal")
 signal main_menu()
