b58dbf82053da6db40d37db85d8b1a62f75402e9
DaemonWhite <daemonwhites@vivaldi.net>
2026-07-20 11:04:21 +0200

    fix: resume menu

--- ui/screens/menu_pause/menu_pause.gd
+++ ui/screens/menu_pause/menu_pause.gd
@@ -1,12 +1,13 @@
 class_name PauseMenu
 extends BaseLayerUi
 	
+func _on_resume_button_pressed() -> void:
+	GameSignals.pause_closed.emit()
+
 func _on_exit_button_pressed() -> void:
 	GameSignals.main_menu.emit()
 	self.close()
 
 func _on_options_button_pressed() -> void:
-	print("option")
-	var settings_menu = UiManager.push_ui("SettingsMenu")
-	settings_menu.open()
+	GameSignals.ui_open_requested.emit("SettingsMenu")
 	
